36efc838933f9df8cee137b9d58cac72f77983cf
adam <adam.kozuch@gmail.com>
2023-05-01 12:54:43 +0200

    Adding current location

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+import { useMap } from 'react-leaflet';
 import Head from 'next/head';
 
 import Layout from '@components/Layout';
@@ -8,10 +10,34 @@ import Button from '@components/Button';
 
 import styles from '@styles/Home.module.scss';
 
-const DEFAULT_CENTER = [52.2297, 21.0122]
+const DEFAULT_CENTER = [52.2297, 21.0122];
 
-const DEFAULT_CENTER_OTHER = [51.2297, 21.0122]
+const DEFAULT_CENTER_OTHER = [51.2297, 21.0122];
 export default function Home() {
+  const [userPosition, setUserPosition] = useState(null);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setUserPosition([position.coords.latitude, position.coords.longitude]);
+        },
+        () => console.error('Error getting current position'),
+        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+      );
+    }
+  }, []);
+  const CustomMap = ({ center }) => {
+    const map = useMap();
+    useEffect(() => {
+      if (center) {
+        map.flyTo(center, map.getZoom());
+      }
+    }, [center, map]);
+
+    return null;
+  };
+
   return (
     <Layout>
       <Head>
@@ -22,27 +48,28 @@ export default function Home() {
 
       <Section>
         <Container>
-          <h1 className={styles.title}>
-            Mapa kontenerów PCK
-          </h1>
+          <h1 className={styles.title}>Mapa kontenerów PCK</h1>
 
           <Map className={styles.homeMap} width="800" height="400" center={DEFAULT_CENTER} zoom={12}>
             {({ TileLayer, Marker, Popup }) => (
               <>
+                <CustomMap center={userPosition ?? DEFAULT_CENTER} />
                 <TileLayer
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                   attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                 />
-                <Marker position={DEFAULT_CENTER}>
-                  <Popup>
-                     https://maps.google.com/?ll=52.2297,21.0122<br />
-                  </Popup>
-                </Marker>
+                {userPosition && (
+                  <Marker position={userPosition}>
+                    <Popup>
+                      Your current location<br />
+                    </Popup>
+                  </Marker>
+                )}
               </>
             )}
           </Map>
         </Container>
       </Section>
     </Layout>
-  )
+  );
 }
